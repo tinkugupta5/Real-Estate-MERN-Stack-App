c7e4249d7ee677e2fb2f1918c301d470eb178569
tinkugupta5 <tinku.gupta4@gmail.com>
2024-07-01 23:51:30 +0530

    makingf routes for checking

--- api/app.js
+++ api/app.js
@@ -1,13 +1,29 @@
 import express from 'express';
 const app = express();
 
-app.use("/api/test",(req,res)=>{
-    res.send("It works")
-})
+// app.use("/api/test",(req,res)=>{
+//     res.send("It works")
+// })
 
-app.listen(8800,()=>{
-    console.log("Server is running")
-})
+// app.use("/api/auth/register",()=>{
+//     req.send("it works");
+// })
+// app.use("/api/auth/login",()=>{
+//     req.send("it works");
+// })
+// app.use("/api/auth/logout",()=>{
+//     req.send("it works");
+// })
+// app.use("/api/posts/",()=>{
+//     req.send("it works");
+// })
+// app.use("/api/posts/12452",()=>{
+//     req.send("it works");
+// })
+
+// app.listen(8800,()=>{
+//     console.log("Server is running")
+// })
 
 // 
 // 12:56
---
+++ api/routes/post.route.js
@@ -0,0 +1,9 @@
+import express from ' express';
+import router from './routes/post.route.js'
+
+const router = express.Router()
+router.get("/test",(req,res)=>{
+    console.log("response router works")
+})
+
+export default router;
