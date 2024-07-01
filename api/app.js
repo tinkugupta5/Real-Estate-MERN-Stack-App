4cedaae60755689e5c7648465fffdc4eaea88b0a
tinkugupta5 <tinku.gupta4@gmail.com>
2024-07-02 00:27:58 +0530

    routes chnages

--- api/app.js
+++ api/app.js
@@ -1,30 +1,12 @@
 import express from 'express';
+import postRoute from './routes/post.route.js';
 const app = express();
 
-// app.use("/api/test",(req,res)=>{
-//     res.send("It works")
-// })
+app.use("/api/posts",postRoute)
 
-// app.use("/api/auth/register",()=>{
-//     req.send("it works");
-// })
-// app.use("/api/auth/login",()=>{
-//     req.send("it works");
-// })
-// app.use("/api/auth/logout",()=>{
-//     req.send("it works");
-// })
-// app.use("/api/posts/",()=>{
-//     req.send("it works");
-// })
-// app.use("/api/posts/12452",()=>{
-//     req.send("it works");
-// })
+app.listen(8800,()=>{
+    console.log("Server is running")
+})
 
-// app.listen(8800,()=>{
-//     console.log("Server is running")
-// })
-
-// 
 // 12:56
 // https://youtu.be/eJ3YysWaP_A?t=776
