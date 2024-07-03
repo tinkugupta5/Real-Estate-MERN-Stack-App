02c5490751e9fae1686f2aaab2c67d085fa5482b
tinkugupta5 <tinku.gupta4@gmail.com>
2024-07-03 23:29:58 +0530

    chanes

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -6,5 +6,8 @@ const router = express.Router()
 router.get("/test",(req,res)=>{
     console.log("response router works")
 })
+router.delete("/test",(req,res)=>{
+    console.log("response router works")
+})
 
 export default router;
