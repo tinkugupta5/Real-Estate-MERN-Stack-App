2d47dd3269064b5f63ef8fbc404ecb5ebe31264d
tinkugupta5 <tinku.gupta4@gmail.com>
2024-07-05 00:26:14 +0530

    register controller created

--- api/app.js
+++ api/app.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import postRoute from './routes/post.route.js';
+import authRoute from "./routes/auth.route.js";
+
 const app = express();
 
 app.use("/api/posts",postRoute)
+app.use("/api/auth",authRoute)
 
 app.listen(8800,()=>{
     console.log("Server is running")
---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const register = (req,res) => {
+    // db operation 
+    console.log("register working ")
+    
+}
+export const login = (req,res) => {
+     // db operation 
+
+}
+export const logout = (req,res) => {
+     // db operation 
+}
