85f4508bd4d73992d81774a17415109f3722344f
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-15 23:37:00 +0530

    auth controller change

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,28 +1,28 @@
 import bcrypt from 'bcrypt';
-import prisma from '../lib/prisma';
+import prisma from '../lib/prisma.js';
 
-export const register = async(req,res) => {
-    // db operation 
-    const {username,email,password} = req.body;
-    console.log("register working")
-   // Hash the password
-   const hashedPassword = bcrypt.hash(password,10)
-   console.log("hashpassword",hashedPassword);
+export const register = async (req, res) => {
+     // db operation 
+     const { username, email, password } = req.body;
+     console.log("register working")
+     // Hash the password
+     const hashedPassword = bcrypt.hash(password, 10)
+     console.log("hashpassword", hashedPassword);
 
-// Create a new user and save to DB
-const newUser = await prisma.user.create({
-     data:{
-          username,
-          email,
-          password:hashedPassword,
-     }
-})
-console.log(newUser);
+     // Create a new user and save to DB
+     const newUser = await prisma.user.create({
+          data: {
+               username,
+               email,
+               password: hashedPassword,
+          }
+     })
+     console.log(newUser);
 }
-export const login = (req,res) => {
- // db operation
+export const login = (req, res) => {
+     // db operation
 }
-export const logout = (req,res) => {
+export const logout = (req, res) => {
      // db operation 
 }
 
