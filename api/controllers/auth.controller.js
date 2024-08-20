7c650f4dc2c562919f297c2829d796bfb17322c4
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-21 01:12:31 +0530

    database url and chnages in model

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,22 +2,36 @@ import bcrypt from 'bcrypt';
 import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
-     // db operation 
-     const { username, email, password } = req.body;
-     console.log("register working")
-     // Hash the password
-     const hashedPassword = bcrypt.hash(password, 10)
-     console.log("hashpassword", hashedPassword);
+     
+          // db operation 
+          const { username, email, password } = req.body;
+          try {
+          console.log("register working")
+          // Hash the password
+          const hashedPassword = await bcrypt.hash(password, 10);
+          console.log("hashpassword", hashedPassword);
 
-     // Create a new user and save to DB
-     const newUser = await prisma.user.create({
-          data: {
-               username,
-               email,
-               password: hashedPassword,
-          }
-     })
-     console.log(newUser);
+          // Create a new user and save to DB
+          const newUser = await prisma.user.create({
+               data: {
+                    username,
+                    email,
+                    password: hashedPassword,
+               }
+          })
+          console.log(newUser);
+          res.status(201).json({
+               message: "User created successfully"
+          })
+
+     } catch (error) {
+          console.log(error);
+
+          res.status(500).json({
+               message:'Failed to created user!'
+          })
+
+     }
 }
 export const login = (req, res) => {
      // db operation
