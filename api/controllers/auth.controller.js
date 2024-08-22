26f30a734fafa97a827ecb9300876d373d96f034
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-23 00:40:36 +0530

    done catch error senario

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -33,7 +33,29 @@ export const register = async (req, res) => {
      }
 }
 export const login = (req, res) => {
-     // db operation
+
+     const {username,password} = req.body;
+     
+     try {
+
+     // check if the user exists
+
+     // check if the password is correct
+
+     // genrate cookie token and send to the user
+
+          
+     } catch (error) {
+
+          console.log(error)
+          res.status(500).json({
+               message:"Failed to login"
+
+          })
+          
+     }
+
+     
 }
 export const logout = (req, res) => {
      // db operation 
