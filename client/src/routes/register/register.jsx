2174d2cc950864288367a8d6c107d1a153ea39e7
tinkugupta5 <tinku.gupta4@gmail.com>
2024-08-30 23:37:59 +0530

    chnages in the

---
+++ client/src/routes/register/register.jsx
@@ -0,0 +1,34 @@
+import "./register.scss";
+import { Link } from "react-router-dom";
+
+function Register() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    const email = formData.get("email")
+    const password = formData.get("password")
+    console.log(username,email,password);
+  }
+
+  return (
+    <div className="register">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Create an Account</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button >Register</button>
+          <Link to="/login">Do you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Register;
