83dbbc855ad8cd24fc5c93aafc4636e66cde04e3
tinkugupta5 <tinku.gupta4@gmail.com>
2024-09-10 23:16:04 +0530

    changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,14 +4,11 @@ import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
 
-     // db operation 
      const { username, email, password } = req.body;
      try {
           console.log("register working")
-          // Hash the password
           const hashedPassword = await bcrypt.hash(password, 10);
           console.log("hashpassword", hashedPassword);
-          // Create a new user and save to DB
           const newUser = await prisma.user.create({
                data: {
                     username,
@@ -26,7 +23,6 @@ export const register = async (req, res) => {
 
      } catch (error) {
           console.log(error);
-
           res.status(500).json({
                message: 'Failed to created user!'
           })
@@ -36,7 +32,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
      const { username, password } = req.body;
      try {
-          // check if the user exists
+         
           const user = await prisma.user.findUnique({
                where: { username }
           })
@@ -44,13 +40,11 @@ export const login = async (req, res) => {
           if (!user) return res.status(401).json({
                message: "Invalid Credentials!"
           })
-          // check if the password is correct
+         
           const isPasswordValid = await bcrypt.compare(password, user.password);
           if (!isPasswordValid) return res.status(401).json({
                message: "Invalid Credentials"
           });
-          // genrate cookie token and send to the user
-          // res.setHeader("Set-Cookie","test=" + "myValue").json("success")
           const age = 1000 * 60 * 60 * 24 * 7
           const token = jwt.sign(
                {
@@ -63,7 +57,6 @@ export const login = async (req, res) => {
           const { password: userPassword, ...userInfo } = user;
           res.cookie("token", token, {
                httpOnly: true,
-               // secure:true
                maxAge: age,
           }).status(200).json(userInfo);
      } catch (error) {
