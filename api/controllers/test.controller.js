5cfe9ecb5f7ab8afc0bf24d8068af893ad4a2e08
tinkugupta5 <tinku.gupta4@gmail.com>
2024-12-03 23:54:07 +0530

    chnages in the

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -52,7 +52,7 @@ export const shouldBeAdmin = async (req, res) => {
     jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
         if (err) return res.status(403).json({ message: "Token is not Valid!" });
         if (!payload.isAdmin) {
-            return res.status(403).json({ message: 'Not Authorized!' });
+            return res.status(403).json({ message: 'Not Authorized' });
         }
 
         res.status(200).json({ message: "You are Authenticated" });
